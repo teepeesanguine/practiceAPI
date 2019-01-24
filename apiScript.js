173b3842202641fc27a075126bd70cd351d1a9ac
cmorgantywls <cmorgan@tywlsbronx.org>
2019-01-24 08:31:11 -0500

    template for AP CSP API practice
    template for AP CSP API practice

---
+++ apiScript.js
@@ -0,0 +1,14 @@
+console.log("connected bruv.")
+
+let theGif = document.querySelector("#showPic")
+
+function getGiphyData(){
+  //I need to:
+  //fetch data from Giphy API   
+  //make a specific gif appear
+}
+
+//Eventual Extensions:
+//Make a random gif show up
+//Add an input so the user can search for a gif
+//Use this function to make lots of different gifs apepar - can you figure out how to do it in a for loop?
